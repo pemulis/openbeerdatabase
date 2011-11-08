982f23e72dbc274974bbf2ee420b3ff65e142241
Tristan Dunn <tristanzdunn@gmail.com>
2011-11-07 22:49:54 -0600

    Ruby 1.9.3 seems to require referencing a constant on self explicitly.

--- lib/custom_pagination.rb
+++ lib/custom_pagination.rb
@@ -22,7 +22,7 @@ module CustomPagination
       column, direction = order.to_s.split(" ", 2)
 
       column.to_s.downcase!
-      column = "id" unless SORTABLE_COLUMNS.include?(column)
+      column = "id" unless self::SORTABLE_COLUMNS.include?(column)
 
       direction.to_s.upcase!
       direction = "ASC" unless %w(ASC DESC).include?(direction)

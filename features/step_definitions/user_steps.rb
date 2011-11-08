99f41705290b67b408751d43bd9829025441caf7
Tristan Dunn <tristanzdunn@gmail.com>
2011-11-08 00:15:47 -0600

    Stop using __send__ in step definitions.

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -21,11 +21,15 @@ When /^I go to the account page for "([^"]+)"$/ do |name|
 end
 
 Then /^the API user with the (public|private) token "([^"]*)" should have (\d+) beers?$/ do |type, token, count|
-  User.__send__("find_by_#{type}_token", token).beers.count.should == count
+  type = "#{type}_token"
+  user = User.where(type => token).first
+  user.beers.count.should == count
 end
 
 Then /^the API user with the (public|private) token "([^"]*)" should have (\d+) (?:brewery|breweries)?$/ do |type, token, count|
-  User.__send__("find_by_#{type}_token", token).breweries.count.should == count
+  type = "#{type}_token"
+  user = User.where(type => token).first
+  user.breweries.count.should == count
 end
 
 Then /^I should see the API tokens for the "([^"]*)" account$/ do |name|

7e55af1286bd39610bf9227d6b920467c8ac4ef0
John Shutt <john.d.shutt@gmail.com>
2013-06-18 17:06:20 -0600

    add test for brewery name uniqueness

--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -5,6 +5,7 @@ describe Brewery do
   it { should belong_to(:user) }
 
   it { should validate_presence_of(:name) }
+  it { should validate_uniqueness_of(:name) }
   it { should ensure_length_of(:name).is_at_most(255) }
   it { should allow_mass_assignment_of(:name) }
 
